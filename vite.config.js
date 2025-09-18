d857c9b31e89ae3de8e395ac586d1ee7e80ca3d7
SalvatoreC94 <salvatorecozzolino.1594@gmail.com>
2025-09-18 14:40:56 +0200

    Fix vite.config.js and prepare deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  base: '/cedit-site/', // 🔑 importantissimo per GitHub Pages
+  base: '/cedit-site/', // 👈 IMPORTANTE per GitHub Pages
+  plugins: [vue()]
 })
